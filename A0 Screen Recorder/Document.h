f55d605e25e85d554cac072783705159640d29d4
Aldrin Martoq <aldrin@martoq.cl>
2012-11-02 18:21:21 -0300

    Initial Commit

---
+++ A0 Screen Recorder/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  A0 Screen Recorder
+//
+//  Created by Aldrin Martoq on 11/2/12.
+//  Copyright (c) 2012 A0. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ A0 Screen Recorder/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  A0 Screen Recorder
+//
+//  Created by Aldrin Martoq on 11/2/12.
+//  Copyright (c) 2012 A0. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
